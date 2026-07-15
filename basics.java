339d1c83ac71ff95ab7dc95225abaeba37dde6db
sayeedur007-design <sayeedur007@gmail.com>
2026-07-15 16:58:44 +0530

    Initial commit

---
+++ basics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class basics{
+    public static void main(String[] args){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the arrray");
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements in array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(arr[0]);
+    }
+}
