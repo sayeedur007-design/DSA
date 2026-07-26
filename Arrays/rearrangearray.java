bec3052f3d9daf52264d81c927fe51784fa9b26c
sayeedur007-design <sayeedur007@gmail.com>
2026-07-26 23:49:19 +0530

    rearranging

---
+++ Arrays/rearrangearray.java
@@ -0,0 +1,20 @@
+// File name: rearrangearray.java
+
+class Solution {
+    public int[] rearrangeArray(int[] arr) {
+        
+    }
+}
+
+public class rearrangearray {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, -1, -3, -4};
+
+        Solution obj = new Solution();
+        int[] ans = obj.rearrangeArray(arr);
+
+        for(int x : ans){
+            System.out.print(x + " ");
+        }
+    }
+}
